1bd59dbe525b41eca6921101a672df27eed3a864
NhanHuynh <nhan123huynh2k3@gmail.com>
2022-10-14 20:19:25 +0700

    new ver

--- main.js
+++ main.js
@@ -109,22 +109,37 @@
 
 //bai 7
 
-function isSquareNumber(n){
-    for(let i=0; i<n;i++){
-        if(n/i==i){
-            return true;
-        }
-    }
-    return false;
+// function isSquareNumber(n){
+//     for(let i=0; i<n;i++){
+//         if(n/i==i){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// let start = Number(prompt('nhap so dau tien: '));
+// let end = Number(prompt('nhap so cuoi cung'));
+// let sum = 0;
+// let count = 0;
+// for(let i=start; i<=end; i++){
+//     if(isSquareNumber(i)) {
+//         sum+=i;
+//         count++;
+//     }
+// }
+// console.log(sum/count);
+
+
+//bai 8
+let arr = [1,3,4,7,9,11,16,77,101,32,51,17,36,45,49,51]
+/*
+arr.sort(function(a,b) {
+    return a-b;
+})
+console.log(arr[arr.length-1]);
+*/
+let temp = arr[0];
+for(let i = 1; i<arr.length-1;i++){
+    if(temp<arr[i+1]) temp = arr[i+1]
 }
-let start = Number(prompt('nhap so dau tien: '));
-let end = Number(prompt('nhap so cuoi cung'));
-let sum = 0;
-let count = 0;
-for(let i=start; i<=end; i++){
-    if(isSquareNumber(i)) {
-        sum+=i;
-        count++;
-    }
-}
-console.log(sum/count);
+console.log(temp)
