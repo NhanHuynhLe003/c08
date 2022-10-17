c19bb120b21e77e19319e79c6bac38ae333f207c
NhanHuynh <nhan123huynh2k3@gmail.com>
2022-10-17 11:25:04 +0700

    updated

--- main.js
+++ main.js
@@ -131,15 +131,144 @@
 
 
 //bai 8
-let arr = [1,3,4,7,9,11,16,77,101,32,51,17,36,45,49,51]
-/*
-arr.sort(function(a,b) {
-    return a-b;
-})
-console.log(arr[arr.length-1]);
-*/
-let temp = arr[0];
-for(let i = 1; i<arr.length-1;i++){
-    if(temp<arr[i+1]) temp = arr[i+1]
+// let arr = [1,3,4,7,9,11,16,77,101,32,51,17,36,45,49,51]
+// let temp = arr[0];
+// for(let i = 1; i<arr.length-1;i++){
+//     if(temp<arr[i+1]) temp = arr[i+1]
+// }
+// console.log(temp)
+
+
+//bai 9
+// let arr = [15, 20, 12, -17, 10, -6, -34, -9, 0, 35, 21, 5];
+// let res = [];
+// arr.sort(function(a,b) {
+//     return a-b;
+// })
+
+// for(let i=0; i<arr.length;i++){
+//     if(arr[i]<0 && arr[i+1] >= 0){
+//         console.log('so am lon nhat la: '+arr[i])
+//     }
+// }
+
+
+//bai 10
+// let nums = [1, 20, 16, -17, 10, -6, -34, -9, 4, 35, 25, 5, 100, 131];
+// function isSquareNumber(n){
+//     for(let i=0; i<n;i++){
+//         if(n/i==i){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// function swap(arr,a,b){
+//     let temp = arr[a];
+//     arr[a] = arr[b];
+//     arr[b] = temp;
+// }
+// function checkSNumberSmallest(nums){
+// for(let i=0; i<nums.length; i++){
+//     for(let j = i+1; j<nums.length-1;j++){
+//         if(!(nums[i]>nums[j])) continue;
+//         if(nums[i]>nums[j]) swap(nums,i,j);
+//     }
+// }
+// for(let i=0;i<nums.length;i++){
+//     if(isSquareNumber(nums[i])) return nums[i];
+//     }
+// }
+// console.log(checkSNumberSmallest(nums));
+
+
+
+//bai 11
+// let nums = [-1,-6,-9,-33,-15,-18];
+// function checkSoDuong(nums){
+//     for(let i = 0; i<nums.length;i++){
+//         if(nums[i]>0) return true;
+//     }
+//     return false
+// }
+
+// if(checkSoDuong(nums))console.log('ton tai so nguyen duong');
+// else console.log('khong ton tai so nguyen duong')
+
+//bai 12
+
+// let nums = [-1,-6,-9,-33,-15,-18];
+// function checkSoDuong(nums){
+//     for(let i = 0; i<nums.length;i++){
+//         if(nums[i]>0) return false;
+//     }
+//     return true;
+// }
+// if(checkSoDuong(nums))console.log('Arr toan am');
+// else console.log('ton tai so duong')
+
+
+
+//bai 13
+// let nums = [1, 20, -17, 10, -6, -34, -9,  35, 5];
+
+// function isExistSquareNum(nums){
+//     function isSquareNumber(n){
+//         for(let i=0; i<n;i++){
+//             if(n/i==i){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     for(let i=0;i<nums.length;i++){
+//         if(isSquareNumber(nums[i])) return true;
+//     }
+//     return false
+// }
+
+// if(isExistSquareNum(nums))console.log('ton tai so chinh phuong');
+// else console.log('khong ton tai so chinh phuong')
+
+
+
+//bai 14
+// let nums = [4,16,25,100,121];
+
+// function isExistSquareNum(nums){
+//     function isSquareNumber(n){
+//         for(let i=0; i<n;i++){
+//             if(n/i==i){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     let count = 1;
+//     for(let i=0;i<nums.length;i++){
+//         if(isSquareNumber(nums[i])) {  
+//         count++;
+//         }
+//     }
+//     if(count === nums.length) return true;
+//     return false
+// }
+
+// if(isExistSquareNum(nums))console.log('toan so chinh phuong');
+// else console.log('co so ko phai so chinh phuong')
+
+//bai 15
+let nums = [1,2,3,5,4]
+function kiemTraTangDan(nums){
+    let count = 1;
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length-1;j++){
+            if(nums[i]<nums[j]) count++;
+        }
+    }
+    if(count === nums.length) return true
+    return false
 }
-console.log(temp)
+
+if(kiemTraTangDan(nums)) console.log('mang tang dan')
